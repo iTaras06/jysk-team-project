25364f14a985883ce6f2d1cb02b8ab5523de0b4d
Vanya <ivanrogacevskiy7@gmail.com>
2026-06-10 14:05:09 +0300

    update

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Footer() {
     return (
         <footer className="w-full bg-[rgba(0,170,173,1)] text-white">
-            <div className="mx-auto max-w-[1920px] px-[104px] pt-[52px] pb-[70px]
+            <div className="mx-auto max-w-[1280px] px-[64px] pt-[32px] pb-[40px]
                             max-[768px]:px-[30px] max-[768px]:pt-[36px] max-[768px]:pb-[48px]
                             max-[640px]:px-[60px] max-[640px]:pt-[24px] max-[640px]:pb-[32px]">
 
@@ -15,94 +15,94 @@ export default function Footer() {
                 max-[640px]:gap-x-[40px]">
 
                     <div className="flex flex-col">
-                        <h3 className="font-bold text-[32px] mb-[34px]
+                        <h3 className="font-bold text-[19px] mb-[20px]
                         max-[768px]:text-[20px]
                         max-[768px]:mb-[20px]
                         max-[640px]:text-[14px]
                         max-[640px]:mb-[12px]">Категорії товарів
                         </h3>
 
-                        <div className="flex flex-col gap-[24px]
+                        <div className="flex flex-col gap-[13px]
                         max-[768px]:gap-[14px]
                         max-[640px]:gap-[10px]">
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Спальня</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Спальня</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Ванна</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Ванна</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Офіс</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Офіс</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Вітальня</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Вітальня</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Кухня та їдальня</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Кухня та їдальня</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Зберігання</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Зберігання</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Для вікон</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Для вікон</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Для саду</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Для саду</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Для дому</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Для дому</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Усі категорії</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Усі категорії</Link>
                         </div>
                     </div>
 
                     <div className="flex flex-col">
-                        <h3 className="font-bold text-[32px] mb-[34px]
+                        <h3 className="font-bold text-[19px] mb-[20px]
                         max-[768px]:text-[20px]
                         max-[768px]:mb-[50px]
                         max-[640px]:text-[14px]
                         max-[640px]:mb-[12px]">Інформація</h3>
 
-                        <div className="flex flex-col gap-[24px]
+                        <div className="flex flex-col gap-[13px]
                         max-[768px]:gap-[14px]
                         max-[640px]:gap-[10px]">
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Зворотній зв’язок</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Зворотній зв’язок</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Магазини і графіки роботи</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Магазини і графіки роботи</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Умови та положення</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Умови та положення</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Доставка</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Доставка</Link>
 
-                        <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Політика конфіденційності</Link>
+                        <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Політика конфіденційності</Link>
                         </div>
                     </div>
 
                     <div className="flex flex-col">
-                        <h3 className="font-bold text-[32px] mb-[34px]
+                        <h3 className="font-bold text-[19px] mb-[20px]
                         max-[768px]:text-[20px]
                         max-[768px]:mb-[50px]
                         max-[640px]:text-[14px]
                         max-[640px]:mb-[12px]">Hyggy</h3>
 
-                        <div className="flex flex-col gap-[24px]
+                        <div className="flex flex-col gap-[13px]
                         max-[768px]:gap-[14px]
                         max-[640px]:gap-[10px]">
-                            <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Про нас</Link>
+                            <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Про нас</Link>
 
-                            <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Робота в Hyggy</Link>
+                            <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Робота в Hyggy</Link>
 
-                            <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Підписатись на розсилку</Link>
+                            <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Підписатись на розсилку</Link>
 
-                            <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Блог</Link>
+                            <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Блог</Link>
 
-                            <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">B2B</Link>
+                            <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">B2B</Link>
 
-                            <Link href="/" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Корисні посилання</Link>
+                            <Link href="/" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Корисні посилання</Link>
                         </div>
                     </div>
 
-                    <div className="max-w-[360px]">
+                    <div className="max-w-[280px]">
 
-                        <h3 className="font-bold text-[32px] mb-[34px]
+                        <h3 className="font-bold text-[19px] mb-[20px]
                             max-[768px]:text-[20px]
                             max-[768px]:mb-[20px]
                             max-[640px]:text-[14px]
                             max-[640px]:mb-[12px]">Центральний офіс</h3>
 
-                        <div className="text-[30px] leading-[1.3]
+                        <div className="text-[17px] leading-[1.3]
                             max-[768px]:text-[16px]
                             max-[640px]:text-[12px]">
                             <p>м. Київ</p>
@@ -110,104 +110,104 @@ export default function Footer() {
                             <p>123456</p>
                         </div>
 
-                        <div className="mt-[48px]
+                        <div className="mt-[30px]
                             max-[768px]:mt-[28px]
                             max-[640px]:mt-[18px]">
                             
-                            <h4 className="font-bold text-[30px]
+                            <h4 className="font-bold text-[16px]
                             max-[768px]:text-[16px]
                             max-[640px]:text-[12px]">HYGGY B2B</h4>
 
                             <div className="mt-[12px] flex flex-col gap-[10px]
                             max-[640px]:gap-[6px]">
-                                <Link href="tel:+380123456789" className="underline underline-offset-8 text-[30px] max-[768px]:text-[16px] max-[768px]:underline-offset-6 max-[640px]:text-[12px] max-[640px]:underline-offset-4">Тел: +380123456789</Link>
+                                <Link href="tel:+380123456789" className="underline underline-offset-8 text-[17px] max-[768px]:text-[16px] max-[768px]:underline-offset-6 max-[640px]:text-[12px] max-[640px]:underline-offset-4">Тел: +380123456789</Link>
 
-                                <Link href="mailto:b2b@hyggy.com" className="text-[30px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Імейл: b2b@hyggy.com</Link>
+                                <Link href="mailto:b2b@hyggy.com" className="text-[17px] max-[768px]:text-[16px] max-[640px]:text-[12px]">Імейл: b2b@hyggy.com</Link>
 
-                                <Link href="/" className="underline underline-offset-5 font-bold text-[30px] max-[768px]:text-[16px] max-[768px]:underline-offset-4 max-[640px]:text-[12px] max-[640px]:underline-offset-2">Зв&apos;яжіться з нами</Link>
+                                <Link href="/" className="underline underline-offset-5 font-bold text-[17px] max-[768px]:text-[16px] max-[768px]:underline-offset-4 max-[640px]:text-[12px] max-[640px]:underline-offset-2">Зв&apos;яжіться з нами</Link>
                             </div>
                         </div>
 
-                        <div className="mt-[48px]
+                        <div className="mt-[30px]
                             max-[768px]:mt-[28px]
                             max-[640px]:mt-[18px]">
                             
-                            <h4 className="font-bold text-[30px]
+                            <h4 className="font-bold text-[16px]
                             max-[768px]:text-[16px]
                             max-[640px]:text-[12px]">Зворотній зв’язок:</h4>
 
                             <div className="mt-[12px] flex flex-col gap-[10px]
                             max-[640px]:gap-[6px]">
-                                <Link href="mailto:email@hyggy.com" className="underline underline-offset-8 text-[30px] max-[768px]:text-[16px] max-[768px]:underline-offset-6 max-[640px]:text-[12px] max-[640px]:underline-offset-4">email@hyggy.com</Link>
+                                <Link href="mailto:email@hyggy.com" className="underline underline-offset-8 text-[17px] max-[768px]:text-[16px] max-[768px]:underline-offset-6 max-[640px]:text-[12px] max-[640px]:underline-offset-4">email@hyggy.com</Link>
 
-                                <Link href="tel:+380123456789" className="underline underline-offset-8 text-[30px] max-[768px]:text-[16px] max-[768px]:underline-offset-6 max-[640px]:text-[12px] max-[640px]:underline-offset-4">Тел: +380123456789</Link>
+                                <Link href="tel:+380123456789" className="underline underline-offset-8 text-[17px] max-[768px]:text-[16px] max-[768px]:underline-offset-6 max-[640px]:text-[12px] max-[640px]:underline-offset-4">Тел: +380123456789</Link>
 
-                                <Link href="/" className="underline underline-offset-5 font-bold text-[30px] max-[768px]:text-[16px] max-[768px]:underline-offset-4 max-[640px]:text-[12px] max-[640px]:underline-offset-2">Зв&apos;яжіться з нами</Link>
+                                <Link href="/" className="underline underline-offset-5 font-bold text-[17px] max-[768px]:text-[16px] max-[768px]:underline-offset-4 max-[640px]:text-[12px] max-[640px]:underline-offset-2">Зв&apos;яжіться з нами</Link>
                             </div>
                         </div>
                     </div>
                 </div>
 
-                <div className="mt-[210px] flex justify-between items-end
+                <div className="mt-[80px] flex justify-between items-end
                     max-[768px]:mt-[130px]
                     max-[640px]:mt-[54px]
                     max-[640px]:flex-col
                     max-[640px]:items-center
                     max-[640px]:gap-[24px]">
 
-                    <div className="order-2 flex items-center gap-[58px]
+                    <div className="order-2 flex items-center gap-[34px]
                         max-[768px]:gap-[28px]
                         max-[640px]:order-1
                         max-[640px]:justify-center
                         max-[640px]:w-full
                         max-[640px]:gap-[51px]">
 
-                        <Link href="/"><Image src="/icons/telega.png" alt="" width={61} height={61} className=" w-[61px] h-[61px]
+                        <Link href="/"><Image src="/icons/telega.png" alt="" width={36} height={36} className=" w-[36px] h-[36px]
                             max-[768px]:w-[40px]
                             max-[768px]:h-[40px]
                             max-[640px]:w-[30px]
                             max-[640px]:h-[30px]"/></Link>
 
-                        <Link href="/"> <Image src="/icons/insta.png" alt="" width={55} height={56} className=" w-[55px] h-[56px]
+                        <Link href="/"> <Image src="/icons/insta.png" alt="" width={32} height={33} className=" w-[32px] h-[33px]
                             max-[768px]:w-[35px]
                             max-[768px]:h-[36px]
                             max-[640px]:w-[26px]
                             max-[640px]:h-[27px]"/></Link>
 
-                        <Link href="/"><Image src="/icons/fbook.png" alt="" width={55} height={56} className=" w-[55px] h-[56px]
+                        <Link href="/"><Image src="/icons/fbook.png" alt="" width={32} height={33} className=" w-[32px] h-[33px]
                             max-[768px]:w-[35px]
                             max-[768px]:h-[36px]
                             max-[640px]:w-[26px]
                             max-[640px]:h-[27px]"/>
                         </Link>
 
-                        <Link href="/"><Image src="/icons/ytube.png" alt="" width={63} height={50} className=" w-[63px] h-[50px]
+                        <Link href="/"><Image src="/icons/ytube.png" alt="" width={37} height={29} className=" w-[37px] h-[29px]
                             max-[768px]:w-[40px]
                             max-[768px]:h-[33px]
                             max-[640px]:w-[30px]
                             max-[640px]:h-[25px]"/></Link>
                     </div>
 
-                    <div className="order-1 flex items-center gap-[38px] 
+                    <div className="order-1 flex items-center gap-[22px]
                         max-[768px]:gap-[22px]
                         max-[640px]:order-2
                         max-[640px]:justify-center
                         max-[640px]:w-full
                         max-[640px]:gap-[50px]">
-                            
-                        <Image src="/icons/visa.png" alt="" width={137} height={80} className=" w-[137px] h-[80px] rounded-[6px]
+
+                        <Image src="/icons/visa.png" alt="" width={80} height={47} className=" w-[80px] h-[47px] rounded-[6px]
                             max-[768px]:w-[80px]
                             max-[768px]:h-[47px]
                             max-[640px]:w-[62px]
                             max-[640px]:h-[36px]"/>
 
-                        <Image src="/icons/mcard.png" alt="Mastercard" width={116} height={80} className=" w-[116px] h-[80px] rounded-[6px]
+                        <Image src="/icons/mcard.png" alt="Mastercard" width={68} height={47} className=" w-[68px] h-[47px] rounded-[6px]
                             max-[768px]:w-[68px]
                             max-[768px]:h-[47px]
                             max-[640px]:w-[53px]
                             max-[640px]:h-[36px]"/>
 
-                        <Image src="/icons/gpay.png" alt="Google Pay" width={129} height={80} className=" w-[129px] h-[80px] rounded-[6px]
+                        <Image src="/icons/gpay.png" alt="Google Pay" width={75} height={47} className=" w-[75px] h-[47px] rounded-[6px]
                             max-[768px]:w-[75px]
                             max-[768px]:h-[47px]
                             max-[640px]:w-[59px]
--- src/components/layout/Prefooter.tsx
+++ src/components/layout/Prefooter.tsx
@@ -2,9 +2,9 @@ import Image from "next/image";
 
 export default function Prefooter() {
     return (
-    <section className="w-full mx-auto h-[755px] text-[rgba(35, 31, 32, 1)]">
+    <section className="w-full mx-auto h-[638px] text-[rgba(35, 31, 32, 1)]">
 
-    <div className="h-[240px] border-b border-black/50 bg-white shadow-[0px_4px_10px_rgba(0,0,0,0.25)]  max-[768px]:h-[300px] max-[640px]:h-[400px]">
+    <div className="h-[198px] border-b border-black/50 bg-white shadow-[0px_4px_10px_rgba(0,0,0,0.25)]  max-[768px]:h-[300px] max-[640px]:h-[400px]">
 
         <div className="mx-auto flex h-full items-center justify-between px-[10%]
             max-[768px]:px-[25px]
@@ -14,29 +14,29 @@ export default function Prefooter() {
             max-[640px]:gap-[17px]
             max-[640px]:px-[40px]">
 
-            <div className="flex w-[415px] flex-col items-center text-center">
+            <div className="flex w-[374px] flex-col items-center text-center">
                 <Image src="/icons/chair.png" alt="chair"
-                    width={80}
-                    height={63}
+                    width={66}
+                    height={52}
                     className="shrink-0 max-[640px]:h-[41px] max-[640px]:w-[52px]"/>
-                
-                <p className="mt-5 text-[16px] leading-[1.2]
+
+                <p className="mt-4 text-[15px] leading-[1.2]
                     max-[768px]:text-[14px]
                     max-[640px]:mt-3
                     max-[640px]:w-full
                     max-[640px]:text-[12px]">
                     Різноманітний вибір сучасних рішень для декорування
-                    та облаштування як інтер'єру, так і екстер'єру.
+                    та облаштування як інтер&apos;єру, так і екстер&apos;єру.
                 </p>
             </div>
 
-            <div className="flex w-[415px] flex-col items-center text-center">
+            <div className="flex w-[374px] flex-col items-center text-center">
                 <Image src="/icons/flag.png" alt="flag"
-                    width={57}
-                    height={67}
+                    width={47}
+                    height={55}
                     className="shrink-0 max-[640px]:h-[41px] max-[640px]:w-[35px]"/>
 
-                <p className="mt-5 text-[16px] leading-[1.2]
+                <p className="mt-4 text-[15px] leading-[1.2]
                     max-[768px]:text-[14px]
                     max-[640px]:mt-3
                     max-[640px]:w-full
@@ -45,13 +45,13 @@ export default function Prefooter() {
                 </p>
             </div>
 
-            <div className="flex w-[415px] flex-col items-center text-center">
+            <div className="flex w-[374px] flex-col items-center text-center">
                 <Image src="/icons/sheet.png" alt="sheet"
-                    width={58}
-                    height={71}
+                    width={48}
+                    height={58}
                     className="shrink-0 max-[640px]:h-[49px] max-[640px]:w-[37px]"/>
 
-                <p className="mt-5 text-[16px] leading-[1.2]
+                <p className="mt-4 text-[15px] leading-[1.2]
                     max-[768px]:text-[14px]
                     max-[640px]:mt-3
                     max-[640px]:w-full
@@ -65,24 +65,24 @@ export default function Prefooter() {
     </div>
 
 
-    <div className="relative h-[550px] bg-[rgba(224, 224, 224, 1)]
+    <div className="relative h-[440px] bg-[rgba(224, 224, 224, 1)]
         max-[768px]:h-[390px]
         max-[640px]:h-[324px]">
 
-        <div className="mx-auto flex h-full max-w-[1600px] flex-col items-center pt-[60px]
+        <div className="mx-auto flex h-full max-w-[1408px] flex-col items-center pt-[50px]
             max-[768px]:pt-[40px]
             max-[768px]:px-[40px]
             max-[640px]:px-[40px]
             max-[640px]:pt-[20px]">
 
-            <h2 className="text-center text-[28px] font-bold leading-[1.2]
+            <h2 className="text-center text-[23px] font-bold leading-[1.2]
                 max-[768px]:text-[20px]
                 max-[640px]:text-[16px]">
                 Підпишись на розсилку новин та отримай код на
                 безкоштовну доставку для свого першого замовлення!
             </h2>
 
-            <p className="mt-10 w-[1400px] text-center text-[24px] leading-[1.2]
+            <p className="mt-7 w-[1155px] text-center text-[20px] leading-[1.2]
                 max-[768px]:mt-6
                 max-[768px]:w-[650px]
                 max-[768px]:text-[14px]
@@ -95,14 +95,14 @@ export default function Prefooter() {
                 Введіть свій email нижче та почніть отримувати переваги!
             </p>
 
-            <div className="mt-[70px] flex items-center gap-[36px]
+            <div className="mt-[58px] flex items-center gap-[30px]
                 max-[768px]:mt-[32px]
                 max-[768px]:gap-[12px]
                 max-[640px]:mt-[30px]
                 max-[640px]:flex-col">
 
                 <input type="text" placeholder="Ім'я"
-                    className="h-[89px] w-[390px] rounded-[10px] bg-[#D9D9D9] px-12 text-[32px] text-[#231F2080] outline-none placeholder:text-[#231F2080]
+                    className="h-[74px] w-[321px] rounded-[10px] bg-[#D9D9D9] px-10 text-[26px] text-[#231F2080] outline-none placeholder:text-[#231F2080]
                     max-[768px]:h-[50px]
                     max-[768px]:w-[160px]
                     max-[768px]:px-4
@@ -112,7 +112,7 @@ export default function Prefooter() {
                     max-[640px]:text-[12px]"/>
 
                 <input type="email" placeholder="E-mail"
-                    className="h-[89px] w-[810px] rounded-[10px] bg-[#D9D9D9] px-12 text-[32px] text-[#231F2080] outline-none placeholder:text-[#231F2080]
+                    className="h-[74px] w-[668px] rounded-[10px] bg-[#D9D9D9] px-10 text-[26px] text-[#231F2080] outline-none placeholder:text-[#231F2080]
                     max-[768px]:h-[50px]
                     max-[768px]:w-[280px]
                     max-[768px]:px-4
@@ -121,7 +121,7 @@ export default function Prefooter() {
                     max-[640px]:w-[260px]
                     max-[640px]:text-[12px]"/>
 
-                <button className="h-[89px] w-[390px] rounded-[10px] bg-[#00AAAD] text-[32px] font-bold    text-white
+                <button className="h-[74px] w-[321px] rounded-[10px] bg-[#00AAAD] text-[26px] font-bold    text-white
                     max-[768px]:h-[50px]
                     max-[768px]:w-[200px]
                     max-[768px]:text-[18px]
@@ -133,7 +133,7 @@ export default function Prefooter() {
             </div>
         </div>
 
-        <div className="absolute bottom-[126px] left-0 h-[1px] w-full bg-black/50
+        <div className="absolute bottom-[105px] left-0 h-[1px] w-full bg-black/50
             max-[768px]:bottom-[80px]
             max-[640px]:bottom-[-15px]"/>
         </div>
